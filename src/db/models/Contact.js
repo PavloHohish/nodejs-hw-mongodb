1cc91510e042161908ed3e3f7c291dff857f7a34
PavloHohish <pasha.steam@ukr.net>
2024-09-05 18:28:57 +0300

    first step

---
+++ src/db/models/Contact.js
@@ -0,0 +1,35 @@
+import { Schema, model } from 'mongoose';
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['personal', 'home'],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const ContactCollection = model('contact', contactSchema);
